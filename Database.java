6fc869a3a12a8e5bd49fac8638e36de09fee088a
MrKaif431 <kaif.siswa@gmail.com>
2025-11-26 23:02:42 +0530

    Add files via upload

---
+++ Database.java
@@ -0,0 +1,20 @@
+// FOR DATABASE CONNECTION
+
+package bank.management.System;
+import java.sql.*;
+
+public class Database {
+    Connection connection;
+    Statement statement;
+
+    public Database(){
+        try{
+            connection = DriverManager.getConnection("jdbc:mysql://localhost:3306/BankSystem","root","Mdkaif@1234");
+            statement = connection.createStatement();
+
+        }catch (Exception e){
+            e.printStackTrace();
+
+        }
+    }
+}
